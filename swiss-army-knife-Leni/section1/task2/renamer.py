236f056d1118d1eba3a82fe510b13f53619dcb42
Leni <helenagonzalez@mail.weber.edu>
2026-05-29 00:46:50 -0600

    Completed Section 1 tasks

---
+++ swiss-army-knife-Leni/section1/task2/renamer.py
@@ -0,0 +1,13 @@
+import os
+
+files = [f for f in os.listdir('.') if not f.startswith('.')]
+
+oldNum = 8230
+newNum = 0
+newName = "Hawaii_trip_"
+
+
+for f in files:
+    oldnum += oldNum
+    newNum +=newNnum
+    os.rename("IMG_" + str(oldNum) + ".jpg", newName + str(num) + ".jpg")
